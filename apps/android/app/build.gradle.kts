dea9c6b33ef9323ab4e7ef684c8728bab9bc3d15
pa-mi-su <pamisu@protonmail.com>
2026-07-09 09:44:13 -0400

    Bump Android version to 1.0.14

--- apps/android/app/build.gradle.kts
+++ apps/android/app/build.gradle.kts
@@ -63,7 +63,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = ciVersionCode
-        versionName = "1.0.13"
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
